52363ec6273384cf3a3acb0427ec7e8aeb50f585
BalazsPh21 <balazsph@outlook.hu>
2024-01-05 10:49:54 +0100

    Cleaned up launch files

--- ars_bringup/launch/ars_bringup.launch.py
+++ ars_bringup/launch/ars_bringup.launch.py
@@ -3,102 +3,104 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration
-
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 
 import xacro
 
 
 def generate_launch_description():
-    # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-
-    pkg_project_description = os.path.join(
-        get_package_share_directory('ars_description')
-    )
-
-    xacro_file = os.path.join(pkg_project_description,
-                              'urdf',
-                              'arc.urdf')
-
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml(), 'use_sim_time': use_sim_time}
-
-    node_robot_state_publisher = Node(
+    pkg_ars_bringup = get_package_share_directory('ars_bringup')
+    pkg_ars_description = get_package_share_directory('ars_description')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    
+    # Process the URDF file
+    xacro_file = os.path.join(pkg_ars_description,'urdf','arc.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml(),  'use_sim_time': True}
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params],
+        parameters=[params]
     )
 
-    ignition_spawn_entity = Node(
-        package='ros_gz_sim',
+    # Setup to launch the simulator and Gazebo world
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(
+        package='ros_gz_sim', 
         executable='create',
-        output='screen',
-        arguments=['-string', doc.toxml(),
-                   '-name', 'cartpole',
-                   '-allow_renaming', 'true'],
-    )
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'arc',
+            '-allow_remapping', 'true'],
         output='screen'
     )
 
-
-    load_ack_steer_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'ackermann_steering_controller'],
-        output='screen'
+    # Spawn ros2_controller ackermann_steering_controller
+    ackermann_steering_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ackermann_steering_controller"],
     )
 
-    load_imu_sensor_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'imu_sensor_broadcaster'],
-        output='screen'
+    # Spawn ros2_controller joint_state_broadcaster
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
     )
 
-    # Bridge
+    # Launch ROS Gazebo Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+        parameters=[{
+            'config_file': os.path.join(pkg_ars_bringup, 'config', 'ros_gz_bridge.yaml')
+        }],
         output='screen'
     )
 
+    # Launch RViz
+    rviz = Node(
+       package='rviz2',
+       executable='rviz2',
+       arguments=['-d', os.path.join(pkg_ars_bringup, 'config', 'ars.rviz')],
+       condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    # Launch!
     return LaunchDescription([
         bridge,
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('ros_gz_sim'),
-                              'launch', 'gz_sim.launch.py')]),
-            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
+        gazebo,
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=ignition_spawn_entity,
-                on_exit=[load_joint_state_controller],
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster],
             )
         ),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=load_joint_state_controller,
-                on_exit=[load_ack_steer_controller],
+                target_action=joint_state_broadcaster,
+                on_exit=[ackermann_steering_controller],
             )
         ),
-        node_robot_state_publisher,
-        ignition_spawn_entity,
-        load_imu_sensor_broadcaster,
-        # Launch Arguments
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='If true, use simulated clock'),
+        robot_state_publisher,
+        spawn_entity,
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        rviz
     ])
