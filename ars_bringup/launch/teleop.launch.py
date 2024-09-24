ccc2c814f31ae8f5af1ab13c0ad501230978a55a
BalazsPh21 <balazsph@outlook.hu>
2024-09-24 21:59:57 +0200

    3D LiDAR works

--- ars_bringup/launch/teleop.launch.py
+++ ars_bringup/launch/teleop.launch.py
@@ -1,15 +1,6 @@
-import os
-
-
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
-from launch.substitutions import LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.event_handlers import OnProcessExit
 
-import xacro
 
 
 def generate_launch_description():
